e8b791bc9d6ad80e1807e66e9db27a059e2556bc
Francis Williams <francis@fwilliams.info>
2019-02-08 16:35:17 -0500

    add a setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fml",
+    version="0.0.1",
+    author="Francis Williams",
+    author_email="francis@fwilliams.info",
+    description=" FML (Francis' Machine-Learnin' Library) - A collection of utilities for machine learning tasks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/fwilliams/fml",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
